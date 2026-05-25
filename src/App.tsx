de5ad337877a65f659810417c4bdd3cd5438965e
Saran <saran@Mac.lan>
2026-05-25 13:43:51 +0530

    Switch to HashRouter for GitHub Pages SPA routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import DashboardLayout from "./components/DashboardLayout";
 import Overview from "./pages/Overview";
